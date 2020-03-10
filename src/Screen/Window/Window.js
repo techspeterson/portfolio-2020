ef4da3784442c16357138a1fca126bd2a96403cc
techspeterson <tpeterson.engsci@gmail.com>
2020-03-10 21:05:58 +1100

    x button is clickable

--- src/Screen/Window/Window.js
+++ src/Screen/Window/Window.js
@@ -3,15 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from "./Window.module.css";
 
 class Window extends React.Component {
+  onClick = () => {
+    console.log("closing!");
+  }
+
   render() {
     return (
       <div className={styles.windowContainer}>
         <div className={styles.topBar}>
           <span>{this.props.title}</span>
           <div className={styles.windowControls}>
-            <FontAwesomeIcon icon="window-minimize" />
-            <FontAwesomeIcon icon="window-maximize" />
-            <FontAwesomeIcon icon="window-close" />
+            <FontAwesomeIcon icon="window-minimize" className={styles.windowControlIcon} />
+            <FontAwesomeIcon icon="window-maximize" className={styles.windowControlIcon} />
+            <FontAwesomeIcon icon="window-close" className={styles.windowControlIcon} onClick={this.onClick} />
           </div>
         </div>
         <div className={styles.windowInner}>
