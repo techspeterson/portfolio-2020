7b85e103a5ad70011e8f56684f5fcb4cf9138dab
techspeterson <tpeterson.engsci@gmail.com>
2020-03-11 17:28:26 +1100

    filter on bg

--- src/Screen/Screen.js
+++ src/Screen/Screen.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Clock from 'react-live-clock';
-import Battery from 'react-device-battery';
 import { Palette } from "react-palette";
 
 import styles from "./Screen.module.css";
 import DesktopIcon from "./DesktopIcon/DesktopIcon";
+import Taskbar from "./Taskbar/Taskbar";
 import { openWindow } from "../store";
 
 function mapStateToProps(state) {
@@ -32,55 +30,6 @@ class Screen extends React.Component {
     }
   }
 
-  renderBattery = ({ battery }) => {
-    let batteryIcon = "battery-";
-    if (battery < 5) {
-      batteryIcon += "empty"
-    } else if (battery < 30) {
-      batteryIcon += "quarter"
-    } else if (battery < 60) {
-      batteryIcon += "half"
-    } else if (battery < 80) {
-      batteryIcon += "three-quarters"
-    } else {
-      batteryIcon += "full"
-    }
-    return <FontAwesomeIcon icon={batteryIcon} />
-  }
-
-  renderTaskbarWindows = () => {
-    const { currentWindows } = this.props;
-    if (currentWindows.length) {
-      return currentWindows.map((window, index) => {
-        return <div key={index} className={styles.taskbarTab}>
-          <FontAwesomeIcon icon={window.icon} className={styles.tabIcon} />
-          {window.title}
-        </div>;
-      });
-    }
-  }
-
-  renderTaskbar = (palette) => {
-    return (
-      <div className={styles.taskbar} style={{ color: palette.vibrant }}>
-        <FontAwesomeIcon icon={["fab", "windows"]} />
-        <div className={styles.taskbarTabs}>
-          {this.renderTaskbarWindows()}
-        </div>
-        <div className={styles.taskbarRight}>
-          <FontAwesomeIcon icon="wifi" />
-          <Battery
-            render={this.renderBattery}
-            onChange={(battery) => {
-              console.log(battery)
-            }}
-          />
-          <Clock format={"h:mm A"} ticking={true} />
-        </div>
-      </div>
-    );
-  }
-
   render() {
     return (
       <Palette src={this.props.bgURL}>
@@ -89,16 +38,20 @@ class Screen extends React.Component {
             className={styles.desktop}
             style={{ background: `url(${this.props.bgURL})` }}
           >
-            <DesktopIcon icon="user" name="Profile" onClick={this.openAbout} colour={data.vibrant} />
-            <DesktopIcon icon="pencil-alt" name="Projects" colour={data.vibrant} />
-            <DesktopIcon icon="paper-plane" name="Contact" colour={data.vibrant} />
-            <div className={styles.windowContainer}>
-              {this.renderTopWindow()}
+            <div className={styles.desktopInner}>
+              <DesktopIcon icon="user" name="Profile" onClick={this.openAbout} colour={data.vibrant} />
+              <DesktopIcon icon="pencil-alt" name="Projects" colour={data.vibrant} />
+              <DesktopIcon icon="paper-plane" name="Contact" colour={data.vibrant} />
+              <div className={styles.windowContainer}>
+                {this.renderTopWindow()}
+              </div>
+              <DesktopIcon icon="code" name="Credits" className={styles.credits} colour={data.vibrant} />
+              <Taskbar palette={data} />
             </div>
-            <DesktopIcon icon="code" name="Credits" className={styles.credits} colour={data.vibrant} />
-            {this.renderTaskbar(data)}
+
           </div>
-        )}
+        )
+        }
       </Palette>
     );
   }
---
+++ src/Screen/Taskbar/Taskbar.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styles from "./Taskbar.module.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Clock from 'react-live-clock';
+import Battery from 'react-device-battery';
+import { connect } from "react-redux";
+
+function mapStateToProps(state) {
+  return {
+    currentWindows: state.currentWindows
+  };
+}
+
+class Taskbar extends React.Component {
+  renderBattery = ({ battery }) => {
+    let batteryIcon = "battery-";
+    if (battery < 5) {
+      batteryIcon += "empty"
+    } else if (battery < 30) {
+      batteryIcon += "quarter"
+    } else if (battery < 60) {
+      batteryIcon += "half"
+    } else if (battery < 80) {
+      batteryIcon += "three-quarters"
+    } else {
+      batteryIcon += "full"
+    }
+    return <FontAwesomeIcon icon={batteryIcon} />
+  }
+
+  renderTaskbarWindows = () => {
+    const { currentWindows } = this.props;
+    if (currentWindows.length) {
+      return currentWindows.map((window, index) => {
+        return <div key={index} className={styles.taskbarTab}>
+          <FontAwesomeIcon icon={window.icon} className={styles.tabIcon} />
+          {window.title}
+        </div>;
+      });
+    }
+  }
+
+  render() {
+    const { palette } = this.props;
+
+    return (
+      <div className={styles.taskbar} style={{ color: palette.vibrant }}>
+        <FontAwesomeIcon icon={["fab", "windows"]} />
+        <div className={styles.taskbarTabs}>
+          {this.renderTaskbarWindows()}
+        </div>
+        <div className={styles.taskbarRight}>
+          <FontAwesomeIcon icon="wifi" />
+          <Battery
+            render={this.renderBattery}
+            onChange={(battery) => {
+              console.log(battery)
+            }}
+          />
+          <Clock format={"h:mm A"} ticking={true} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Taskbar);
