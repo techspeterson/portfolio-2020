0ce94ee43febd3c05e9879573eedb04a14bb8c16
techspeterson <tpeterson.engsci@gmail.com>
2020-03-11 15:51:21 +1100

    refactoring, added loading screen

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUser, faPencilAlt, faPaperPlane, faCode, faBatteryFull, faBatteryThreeQuarters, faBatteryHalf, faBatteryQuarter, faBatteryEmpty, faWifi, faWindowMaximize, faWindowClose, faWindowMinimize, faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import { faUser, faPencilAlt, faPaperPlane, faCode, faBatteryFull, faBatteryThreeQuarters, faBatteryHalf, faBatteryQuarter, faBatteryEmpty, faWifi, faWindowMaximize, faWindowClose, faWindowMinimize, faFilePdf, faAtom } from "@fortawesome/free-solid-svg-icons";
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { Provider } from "react-redux";
-// import axios from "axios";
-import './App.css';
+import axios from "axios";
+import styles from './App.module.css';
 
 import store from "./store";
 import Screen from "./Screen/Screen";
+import StartupScreen from "./Screen/StartupScreen";
 
-library.add(faUser, faPencilAlt, faPaperPlane, faCode, faBatteryFull, faBatteryThreeQuarters, faBatteryHalf, faBatteryQuarter, faBatteryEmpty, faWifi, faWindowMaximize, faWindowClose, faWindowMinimize, faFilePdf, fab);
+library.add(faUser, faPencilAlt, faPaperPlane, faCode, faBatteryFull, faBatteryThreeQuarters, faBatteryHalf, faBatteryQuarter, faBatteryEmpty, faWifi, faWindowMaximize, faWindowClose, faWindowMinimize, faFilePdf, faAtom, fab);
 
 class App extends React.Component {
+  state = {
+    bgURL: null
+  }
+
+  componentDidMount() {
+    axios.get("https://picsum.photos/1000/700").then(res => {
+      const id = res.headers["picsum-id"];
+      axios.get(`https://picsum.photos/id/${id}/info`).then(res => {
+        this.setState({ bgURL: `https://picsum.photos/id/${id}/1000/700` });
+      });
+    });
+  }
+
+  renderScreen = () => {
+    if (this.state.bgURL) {
+      return (
+        <Screen bgURL={this.state.bgURL} />
+      )
+    }
+    else {
+      return (
+        <StartupScreen />
+      )
+    }
+  }
+
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <Screen />
+        <div className={styles.monitor}>
+          <div className={styles.screenContainer}>
+            {this.renderScreen()}
+          </div>
         </div>
       </Provider>
     );
--- src/Screen/Screen.js
+++ src/Screen/Screen.js
@@ -3,7 +3,6 @@ import { connect } from "react-redux";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Clock from 'react-live-clock';
 import Battery from 'react-device-battery';
-import axios from "axios";
 import styles from "./Screen.module.css";
 
 import DesktopIcon from "./DesktopIcon/DesktopIcon";
@@ -18,19 +17,6 @@ const mapDispatchToProps = {
 }
 
 class Screen extends React.Component {
-  state = {
-    bgURL: null
-  }
-
-  componentDidMount() {
-    axios.get("https://picsum.photos/1000/700").then(res => {
-      const id = res.headers["picsum-id"];
-      axios.get(`https://picsum.photos/id/${id}/info`).then(res => {
-        this.setState({ bgURL: `https://picsum.photos/id/${id}/1000/700` });
-      });
-    });
-  }
-
   openAbout = () => {
     this.props.openWindow("about");
   }
@@ -91,35 +77,20 @@ class Screen extends React.Component {
     );
   }
 
-  renderLoadingOrScreen = () => {
-    if (this.state.bgURL) {
-      return (
-        <div
-          className={styles.desktop}
-          style={{ background: `url(${this.state.bgURL})` }}
-        >
-          <DesktopIcon icon="user" name="Profile" onClick={this.openAbout} />
-          <DesktopIcon icon="pencil-alt" name="Projects" />
-          <DesktopIcon icon="paper-plane" name="Contact" />
-          <div className={styles.windowContainer}>
-            {this.renderTopWindow()}
-          </div>
-          <DesktopIcon icon="code" name="Credits" className={styles.credits} />
-          {this.renderTaskbar()}
-        </div>
-      )
-    }
-    else {
-      return (
-        <div>Loading...</div>
-      )
-    }
-  }
-
   render() {
     return (
-      <div className={styles.screenContainer}>
-        {this.renderLoadingOrScreen()}
+      <div
+        className={styles.desktop}
+        style={{ background: `url(${this.props.bgURL})` }}
+      >
+        <DesktopIcon icon="user" name="Profile" onClick={this.openAbout} />
+        <DesktopIcon icon="pencil-alt" name="Projects" />
+        <DesktopIcon icon="paper-plane" name="Contact" />
+        <div className={styles.windowContainer}>
+          {this.renderTopWindow()}
+        </div>
+        <DesktopIcon icon="code" name="Credits" className={styles.credits} />
+        {this.renderTaskbar()}
       </div>
     );
   }
