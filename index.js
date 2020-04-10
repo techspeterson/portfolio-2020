a2d7cc8911b394c6c81eae6e3deca6216ccd7048
techspeterson <techspeterson@gmail.com>
2020-04-10 20:25:18 +1000

    client and server bundled together oh god my workflow

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const fetch = require('node-fetch');
+global.fetch = fetch;
+const Unsplash = require('unsplash-js').default;
+const { toJson } = require("unsplash-js");
+require("dotenv").config();
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = 5000;
+
+const unsplash = new Unsplash({ accessKey: process.env.UNSPLASH_ACCESS_KEY });
+
+app.get("/bg", (req, res) => {
+  unsplash.photos.getRandomPhoto({ orientation: "landscape" })
+    .then(toJson)
+    .then(json => {
+      res.json({
+        imageURL: json.urls.raw,
+        url: json.links.html,
+        author: json.user.name
+      });
+    });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
